652a52ef62df678a920de1f4bda336167e602c9d
Patrick Antunes dos Santos <patrickantunes@hotmail.com>
2020-11-29 01:14:24 -0300

    Refinamento do create de ativos e historico de ativos, update de ativos e do models de ativos

--- project_aux_invest/aux-invest-backend/core/models.py
+++ project_aux_invest/aux-invest-backend/core/models.py
@@ -7,30 +7,33 @@ class Investidor(models.Model):
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
 
-class Ativo(models.Model):
-    nome_ativ = models.CharField(max_length=100, blank=False)
-    sigla = models.CharField(max_length=7, blank=False)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
-
-    def __str__(self):
-        return self.nome_ativ + " " + "("+ self.sigla + ")"
-
 class Carteira(models.Model):
     nome_cart = models.CharField(max_length=100, blank=True)
-    ativos = models.ManyToManyField(Ativo)
+    #ativos = models.ManyToManyField(Ativo)
     investidor = models.ForeignKey(Investidor, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome_cart
 
+class Ativo(models.Model):
+    nome_ativ = models.CharField(max_length=100, blank=False)
+    sigla = models.CharField(max_length=7, blank=False)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    lim_sup = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    lim_inf = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    carteira = models.ForeignKey(Carteira, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.nome_ativ + " " + "("+ self.sigla + ")"
+
 class historicoAtivo(models.Model):
     data = models.DateField(blank=False)
-    adj_close = models.DecimalField(max_digits=7,decimal_places=2, blank=False)
-    close = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
-    High = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
-    Low = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
-    Open = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
-    Volume = models.PositiveIntegerField(blank=False)
+    adj_close = models.DecimalField(max_digits=7,decimal_places=2, blank=True, null=True)
+    close = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    High = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    Low = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    Open = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    Volume = models.PositiveIntegerField(blank=True, null=True)
     ativo = models.ForeignKey(Ativo, on_delete=models.PROTECT)
 
     def __str__(self):
--- project_aux_invest/aux-invest-backend/core/serializers.py
+++ project_aux_invest/aux-invest-backend/core/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+import yahoo_fin.stock_info as si
+from datetime import datetime
+import pandas as pd
 from .models import Investidor, Ativo, Carteira, historicoAtivo
 
 class InvestidorSerializer(serializers.ModelSerializer):
@@ -14,14 +17,58 @@ class InvestidorSerializer(serializers.ModelSerializer):
 class AtivoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ativo
-        fields = ['id', 'nome_ativ', 'sigla', 'preco']
+        fields = ['id', 'nome_ativ', 'sigla', 'preco', 'lim_sup', 'lim_inf', 'carteira']
+
+    def create(self, validated_data):
+        instance = Ativo.objects.create(**validated_data)
+        instance.preco = round(si.get_live_price(instance.sigla + '.sa'),2)
+        instance.save()
+
+        return instance
+    
+    def update(self, instance, validated_data):
+        instance.nome_ativ = validated_data.get('nome_ativ', instance.nome_ativ)
+        instance.sigla = validated_data.get('sigla', instance.sigla)
+        instance.preco = validated_data.get('preco', round(si.get_live_price(instance.sigla + '.sa'),2))
+        instance.lim_sup = validated_data.get('lim_sup', instance.lim_sup)
+        instance.lim_inf = validated_data.get('lim_inf', instance.lim_inf)
+
+        instance.save()
+
+        return instance
+
 
 class CarteiraSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carteira
-        fields = ['id', 'nome_cart']
+        fields = ['id', 'nome_cart', 'investidor']
 
 class historicoAtivoSerializer(serializers.ModelSerializer):
     class Meta:
         model = historicoAtivo
-        fields = ['id', 'data', 'adj_close', 'close', 'High', 'Low', 'Open', 'Volume']
+        fields = ['id', 'data', 'adj_close', 'close', 'High', 'Low', 'Open', 'Volume', 'ativo']
+
+    def create(self, validated_data):
+        instance = historicoAtivo.objects.create(**validated_data)
+
+        #Buscando as informações do histórico do ativo no Yahho Finance
+        data_texto = pd.to_datetime(instance.data) + pd.DateOffset(days=1)
+        historico = si.get_data(instance.ativo.sigla + '.sa', start_date=instance.data, end_date=data_texto)
+        print(instance.data)
+        print(type(instance.data))
+
+        instance.adj_close = round(historico.iloc[0,4], 2)
+        instance.close = round(historico.iloc[0,3], 2)
+        instance.High = round(historico.iloc[0,1], 2)
+        instance.Low = round(historico.iloc[0,2], 2)
+        instance.Open = round(historico.iloc[0,0], 2)
+        instance.Volume = round(historico.iloc[0,5], 2)
+        instance.save()
+
+        return instance
+
+
+#data = datetime.strptime(data, '%d/%m/%Y').date()
+#data_texto = data.strftime('%d/%m/%Y')
+#data = datetime.strptime(instance.data, '%d/%m/%Y')
+#instance.data = data
